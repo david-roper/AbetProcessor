0bf318fc1202a95f4d920014d29dbff513426994
david-roper <davidroper321@gmail.com>
2023-10-11 17:17:34 -0400

    created venv

---
+++ abetToCsv.py
@@ -0,0 +1,30 @@
+import os
+import csv
+import shutil
+import subprocess
+import time
+
+
+
+
+print('\n' + 'Searching for ABET databases and re-formatting ABET databases into SQLite format... ' + '\n')
+fileName = []
+workingDir = os.getcwd()
+loop = 0
+DBsFound = []
+for i in os.listdir(os.curdir):                             #find file names
+    if i.__contains__(".ABETdb"):
+        fileName.append(i)
+for i in fileName:                                          #convert each found file to csv files and place them in folder
+    Pcommand = "bash mdb-export-all.sh " + i
+    p = subprocess.Popen(Pcommand, stdout = subprocess.PIPE, close_fds = True, shell = True)
+    stdout, stderr = p.communicate()
+for i in fileName:                                          #move the files to the main folder, and convert each batch of relevant csv files a single .mdb file
+    folderName, fileExt = fileName[loop].split(".")
+    folderName = str(folderName) + str('/')
+    file1, file2, file3, file4 = (str(folderName) + str('tbl_Data.csv')), (str(folderName) + str('tbl_Schedules.csv')), (str(folderName) + str('tbl_Schedule_Notes.csv')), str(folderName) + str('tbl_Version.csv')
+    shutil.copy2(file1, 'tbl_Data.csv')
+    shutil.copy2(file2, 'tbl_Schedules.csv')
+    shutil.copy2(file3, 'tbl_Schedule_Notes.csv')
+    shutil.copy2(file4, 'tbl_Version.csv')
+    dbName, dbExt = fileName[loop].split('.')
